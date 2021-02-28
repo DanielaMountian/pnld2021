42b12dfb08b6d9131b4fdd6af3cd5e1f82609542
Jorge Sallum <jorge@hedra.com.br>
2021-02-28 16:43:27 -0300

    adicionar metadados no fim de cada arquivo

---
+++ _SLIDES/meta.sh
@@ -0,0 +1,62 @@
+echo "% Editora Lunna; Ficção, mistério e fantasia; Conto, crônica e novela; Lima Barreto; Crônicas e contos" >> PNLD0001-V1.tex
+echo "% Mais e Melhores; Ficção, mistério e fantasia; Conto e novela; Julia Lopes de Almeida; Contos e novelas" >> PNLD0002-V1.tex
+echo "% Hedra Educação; Diálogos com a sociologia e com a antropologia; Diário, biografia, autobiografia, relatos, memórias; Antonio Vieira; Sermões" >> PNLD0003-V1.tex
+echo "% Madalena; Ficção, mistério e fantasia; Conto, crônica e novela; Mário de Andrade; Zeppelin: algumas crônicas de Mário de Andrade" >> PNLD0004-V1.tex
+echo "% Circuito; Ficção, mistério e fantasia; Poema; Mário de Andrade; Eu tudoamo: antologia de poemas de Mário de Andrade" >> PNLD0005-V1.tex
+echo "% Ayllon; Ficção, mistério e fantasia; Conto, crônica e novela; Marcus Baccega (Org.); A demanda do Santo Graal" >> PNLD0006-V1.tex
+echo "% Hedra; Diálogos com a sociologia e com a antropologia; Diário, biografia, autobiografia, relatos, memórias; WPA; Nascidos na escravidão: depoimentos norte-americanos" >> PNLD0007-V1.tex
+echo "% Araucária; Ficção, mistério e fantasia; Romance; Fábio Atui; Mare nostrum: Paranã Tipi" >> PNLD0008-V1.tex
+echo "% Editora Lunna; Ficção, mistério e fantasia; Conto, crônica e novela; Akutagawa; Rashômon" >> PNLD0009-V1.tex
+echo "% Acorde; Ficção, mistério e fantasia; Poema; Charles Baudelaire; Pequenos poemas em prosa" >> PNLD0010-V1.tex
+echo "% Relicário; Ficção, mistério e fantasia; Romance; Bram Stoker; Sob o por do sol" >> PNLD0011-V1.tex
+echo "% Hedra Educação; Ficção, mistério e fantasia; Poema; Oliveira de Panelas; Vocação de cantador" >> PNLD0012-V1.tex
+echo "% Ayllon; Ficção, mistério e fantasia; Romance; Daniel Defoe; Robinson Crusoé" >> PNLD0013-V1.tex
+echo "% Acorde; Ficção, mistério e fantasia; Conto; crônica, novela e poema; Maria Firmina dos Reis; Antologia de prosa e versos" >> PNLD0014-V1.tex
+echo "Piseagrama; Diálogos com a sociologia e com a antropologia; Diário, biografia, autobi% ografia, relatos, memórias; Gabriel Soares; Tratado descritivo do Brasil (Seleta)" >> PNLD0015-V1.tex
+echo "EdLab Press; Diálogos com a sociologia e com a antropologia; Diário, biografia, autobi% ografia, relatos, memórias; Luiz Gama; O herói da abolição: a luta de Luiz Gama no império do Brasil" >> PNLD0016-V1.tex
+echo "% Hedra Educação; Ficção, mistério e fantasia; Romance; Robert Louis Stevenson; O médico e o monstro" >> PNLD0017-V1.tex
+echo "% Iluminuras; Ficção, mistério e fantasia; Romance; Heinrich Heine; O Rabi de Bacherach" >> PNLD0018-V1.tex
+echo "% Hedra Educação; Ficção, mistério e fantasia; Poema; Patativa do Assaré; Uma voz do Nordeste" >> PNLD0019-V1.tex
+echo "% Nexus; Diálogos com a sociologia e com a antropologia; Diário, biografia, autobiografia, relatos, memórias; Harriet Ann Jacobs; Incidentes da vida de uma escrava" >> PNLD0020-V1.tex
+echo "% Hedra Educação; Ficção, mistério e fantasia; Conto, crônica e novela; H.P. Lovecraft; O chamado de Cthulhu" >> PNLD0021-V1.tex
+echo "% Hedra Educação; Ficção, mistério e fantasia; Romance; James Joyce; Stephen Heroi" >> PNLD0022-V1.tex
+echo "Hedra Educação; Diálogos com a sociologia e com a antropologia; Diário, biografia, autobi% ografia, relatos, memórias; Fernão Cardim; Tratado da terra e gente do Brasil (Seleta)" >> PNLD0023-V1.tex
+echo "% Circuito; Ficção, mistério e fantasia; Conto, crônica e novela; Monteiro Lobato; Um suplício moderno e outros contos" >> PNLD0024-V1.tex
+echo "% Hedra Educação; Ficção, mistério e fantasia; Poema; Zé Vicente; Poeta popular paraense" >> PNLD0025-V1.tex
+echo "% Lamparina; Ficção, mistério e fantasia; Conto, crônica e novela; Machado de Assis; Pai contra mãe e outros contos" >> PNLD0026-V1.tex
+echo "% Papagaio; Diálogos com a sociologia e com a antropologia; Conto, crônica e novela; Luis Marra; Crônicas do crack" >> PNLD0027-V1.tex
+echo "% Hedra Educação; Ficção, mistério e fantasia; Conto, crônica e novela; Mário de Andrade; O peru de natal e outros contos" >> PNLD0028-V1.tex
+echo "% n-1; Ficção, mistério e fantasia; Mitologia indígena; Anne Ballester Soares (Org.); O surgimento da noite: mitologias Yanomami*" >> PNLD0029-V1.tex
+echo "% Nexus; Ficção, mistério e fantasia; Poema; Orides Fontela; Poemas escolhidos" >> PNLD0030-V1.tex
+echo "% Hedra Educação; Ficção, mistério e fantasia; Poema; Rodolfo Coelho Cavalcante; Circo, teatro e cordel" >> PNLD0031-V1.tex
+echo "% Hedra Educação; Ficção, mistério e fantasia; Conto, crônica e novela; James Joyce; Dublinenses" >> PNLD0032-V1.tex
+echo "% Blooks; Ficção, mistério e fantasia; Poema; Patativa do Assaré; Ispinho e Fulô" >> PNLD0033-V1.tex
+echo "% Hedra Educação; Diálogos com a sociologia e com a antropologia; Diário, biografia, autobiografia, relatos, memórias; Gandavo; História da província de Santa Cruz" >> PNLD0034-V1.tex
+echo "% Escola de Pais; Ficção, mistério e fantasia; Poema; Safo de Lesbos; Hino a Afrodite e outros poemas" >> PNLD0035-V1.tex
+echo "% Escola de Pais; Ficção, mistério e fantasia; Conto, crônica e novela; Marcel Schwob; Vidas imaginárias e outros textos" >> PNLD0036-V1.tex
+echo "% Hedra Educação; Ficção, mistério e fantasia; Romance; James Joyce; Retrato do artista quando Jovem" >> PNLD0037-V1.tex
+echo "Relicário; Diálogos com a sociologia e com a antropologia; Diário, biografia, autobiografia, relato% s, memórias; William Wells Brown; Narrativa de William Wells Brown, escravo fugitivo" >> PNLD0038-V1.tex
+echo "% Papagaio; Ficção, mistério e fantasia; Romance; Strindberg; Gente de Hemsö" >> PNLD0039-V1.tex
+echo "% Blooks; Ficção, mistério e fantasia; Conto, crônica e novela; Strindberg; Sagas" >> PNLD0040-V1.tex
+echo "% Mais e Melhores; Ficção, mistério e fantasia; Conto, crônica e novela; Johann Ludwig Tieck; Feitiço de amor e outros contos" >> PNLD0041-V1.tex
+echo "% Madalena; Ficção, mistério e fantasia; Romance; Liev Tolstói; A morte de Ivan Ílitch" >> PNLD0042-V1.tex
+echo "% EdLab Press; Ficção, mistério e fantasia; Conto, crônica e novela; Mark Twain; Diários de Adão e Eva" >> PNLD0043-V1.tex
+echo "% Hedra Educação; Ficção, mistério e fantasia; Poema; Hesíodo; Teogonia & Trabalhos e dias" >> PNLD0044-V1.tex
+echo "% Hedra; Ficção, mistério e fantasia; Poema; José Martins de Athayde; Tipógrafo, poeta e editor" >> PNLD0045-V1.tex
+echo "% Araucária; Ficção, mistério e fantasia; Romance; Xavier de Maistre; Viagem em volta do meu quarto" >> PNLD0046-V1.tex
+echo "% Hedra Educação; Diálogos com a sociologia e com a antropologia; Folclore brasileiro; Enrique Menezes (Org.); Jazz Rural" >> PNLD0047-V1.tex
+echo "% Piseagrama; Ficção, mistério e fantasia; Conto, crônica e novela; Machado de Assis; O Alienista — O Imortal — A Cartomante" >> PNLD0048-V1.tex
+echo "% Kalinka; Ficção, mistério e fantasia; Romance; Aleksei Tolstoi; Infância de Nikita" >> PNLD0049-V1.tex
+echo "% Kalinka; Ficção, mistério e fantasia; Conto, crônica e novela; Daniela Mountian (Org.); Contos russos infanto juvenis" >> PNLD0050-V1.tex
+echo "% Lamparina; Ficção, mistério e fantasia; Romance; Maria Valéria Rezende; Rio de sonhos" >> PNLD0051-V1.tex
+echo "Alameda; Diálogos com a sociologia e com a antropologia; Diário, biografia, autobiografia, relato% s, memórias; Irineu Franco Perpétuo; História concisa da música clássica brasileira" >> PNLD0052-V1.tex
+echo "% Alameda; Ficção, mistério e fantasia; Romance; Bernardo Kucinski; Júlia – Nos campos confla% grados do Senhor" >> PNLD0053-V1.tex
+echo "Iluminuras; Ficção, mistério e fantasia; Romance; Sidney Rocha; Sofia" >> PNLD0054-V1.tex
+echo "% Livros de Criação; Ficção, mistério e fantasia; Conto, crônica e novela; Nella Larsen; Passando-se" >> PNLD0055-V1.tex
+echo "Livros de Criação; Diálogos com a sociologia e com a antropologia; Diário, biografia, autobi% ografia, relatos, memórias; Sojourner Truth; A narrativa de Sojourner Truth" >> PNLD0056-V1.tex
+echo "% Letra e Imagem; Diálogos com a sociologia e com a antropologia; Diário, biografia, autobiografia, relatos, memórias; Nellie Bly; Dez dias no manicómio" >> PNLD0057-V1.tex
+echo "% Letra e Imagem; Diálogos com a sociologia e com a antropologia; Diário, biografia, autobiografia, relatos, memórias; Nellie Bly; Volta ao mundo em 72 dias" >> PNLD0058-V1.tex
+echo "% Ubu; Ficção, mistério e fantasia; Mitologia indígena; Ádrian Recinos; Popol Vuh" >> PNLD0059-V1.tex
+echo "% Ubu; História da ciência; Tratado científico; Charles Darwin; Origem das espécies" >> PNLD0060-V1.tex
+echo "% Glac Edições; Diálogos com a sociologia e com a antropologia; Narrativa histórica; Claudio Medeiros; História da experiência das epidemias no Brasil" >> PNLD0061-V1.tex
+echo "% n-1; Protagonismo juvenil; Diário, biografia, autobiografia, relatos, memórias; Igor Mendes; Pequena prisão" >> PNLD0062-V1.tex
